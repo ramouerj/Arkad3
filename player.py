ac91a45b3727447ad1060c9275a663459beda668
diegohutter <diegohsc.uerj@gmail.com>
2013-07-14 19:15:42 -0300

    Adicionando jogo no projeto

---
+++ player.py
@@ -0,0 +1,16 @@
+#!usr/bin python
+#-*- coding:utf-8 -*-
+
+from threading import Thread
+
+class Players(Thread):
+
+	def run(self):
+		'''
+		Aqui deve haver um retorno com duas variáveis:
+		y e intensidade
+		'''
+		pass
+
+	def __init__(self):
+		Thread.__init__(self)
